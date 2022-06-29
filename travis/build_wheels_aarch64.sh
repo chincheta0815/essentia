af634341654918e00e25c04f4c50c27838c9dae0
chincheta0815 <Rouven_Weiler@gmx.net>
2022-06-29 09:22:00 +0200

    Update build_wheels_aarch64.sh

--- travis/build_wheels_aarch64.sh
+++ travis/build_wheels_aarch64.sh
@@ -34,7 +34,7 @@ if [[ $WITH_TENSORFLOW ]]; then
     "${PYBIN}/python" waf configure --with-gaia --with-tensorflow --build-static --static-dependencies ${NO_MSSE} --pkg-config-path="${PKG_CONFIG_PATH}"
 else
     PROJECT_NAME='essentia'
-    "${PYBIN}/python" waf configure --with-gaia --build-static --static-dependencies --with-examples ${NO_MSSE} --pkg-config-path="${PKG_CONFIG_PATH}"
+    "${PYBIN}/python" waf configure --with-gaia --build-static --static-dependencies --with-static-examples ${NO_MSSE} --pkg-config-path="${PKG_CONFIG_PATH}"
 fi
 
 "${PYBIN}/python" waf
